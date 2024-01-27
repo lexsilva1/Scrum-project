94fb3a3f910fe8406c4f7559df1e6d5f808affd8
lexsilva1 <jagmsilva@student.dei.uc.pt>
2024-01-27 20:50:56 +0000

    save tasks and delete tasks

--- homescript.js
+++ homescript.js
@@ -3,6 +3,7 @@ window.onload = function () {
     if (username) {
       document.getElementById("login").textContent = username;
     }
+    loadTasks();
   };
 const tasks = document.querySelectorAll('.task')
 const panels = document.querySelectorAll('.panel')
@@ -39,10 +40,25 @@ panels.forEach(panel => { // Adiciona os listeners de drag and drop a um painel
     e.preventDefault()
     const afterElement = getDragAfterElement(panel, e.clientY)
     const task = document.querySelector('.dragging')
+    const panelID = document.getElementById(panel.id)
     if (afterElement == null) {
       panel.appendChild(task)
+      task.status = panel.id;
+      for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id == task.id) {
+          tasks[i].status = panelID;
+        }
+      }
+      saveTasks();
     } else {
       panel.insertBefore(task, afterElement)
+      task.status = panel.id;
+      for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id == task.id) {
+          tasks[i].status = panelID;
+        }
+      }
+      saveTasks();
     }
   })
 })
@@ -63,37 +79,49 @@ document.getElementById('addTask').addEventListener('click', function() {
   var Description = taskDescription.value.trim();
   var Name = taskName.value.trim();
   if (Name.trim() !== '' && Description.trim() !== ''){
-      const task = createTaskElement(Name, Description);
-     document.getElementById('panel1').appendChild(task);
+      const task = createTask(Name, Description);
+      const taskElement =createTaskElement(task);
+     document.getElementById('panel1').appendChild(taskElement);
 
       // Attach drag and drop listeners to the dynamically created task
-      attachDragAndDropListeners(task);
+      attachDragAndDropListeners(taskElement);
 
       // Clear input fields after adding task
       document.getElementById('taskName').value = '';
       document.getElementById('taskDescription').value = '';
   }
+  saveTasks();
 });
 
-function createTaskElement(name, description) {
+function createTask(name, description) {  // Cria uma tarefa com o nome e descrição passados como argumento 
+  const task = {
+  title :name,
+  description: description,
+  identificacao: 'task-' + Date.now(),
+  status: 'panel1'
+  }
+  return task;
+}
+
+function createTaskElement(task) {
   const taskElement = document.createElement('div');
-  const taskId = 'task-' + Date.now(); // Gera Id especifico da tarefa.
-  taskElement.id = taskId; // Define o Id da tarefa.
+  taskElement.id = task.identificacao; // Define o Id da tarefa.
   taskElement.className = 'task';
   taskElement.draggable = true;
-  taskElement.description = description; // Guarda a descrição da tarefa no elemento.
-  taskElement.title = name; // Guarda o nome da tarefa no elemento.
-
+  taskElement.description = task.description; // Guarda a descrição da tarefa no elemento.
+  taskElement.title = task.title;
+  taskElement.status = task.status;
   const postIt = document.createElement('div');
   postIt.className = 'post-it';
 
   const taskTitle = document.createElement('h3');
-  taskTitle.textContent = name;
+  taskTitle.textContent = task.title;
 
   const deleteButton = document.createElement('img');
   deleteButton.src = 'multimedia/green_cross.png';
   deleteButton.className = 'apagarButton';
   deleteButton.addEventListener('click', function () {
+    deleteTask(taskElement.id);
     taskElement.remove();
   });
 
@@ -103,3 +131,41 @@ function createTaskElement(name, description) {
 
   return taskElement;
 }
+function saveTasks() {
+  const tasks = document.querySelectorAll('.task');
+  const tasksArray = [];
+  tasks.forEach(task => {
+      tasksArray.push({
+          identificacao: task.id,
+          title: task.title,
+          description: task.description,
+          status: task.status
+      });
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasksArray));
+}
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  tasks.forEach(task => {
+    const taskElement = createTaskElement(task);
+    const panel = document.getElementById(task.status);
+    panel.appendChild(taskElement);
+    attachDragAndDropListeners(taskElement);
+  });
+}
+function deleteTask(id) {
+  var task = document.getElementById(id);
+  task.remove();
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+for (var i = 0; i < tasks.length; i++) {
+  if (tasks[i].id == id) {
+    removeItemFromArr(tasks, tasks[i]);
+  }
+  saveTasks();
+}
+}
+window.onclose = function () { 
+  saveTasks();
+}
+
+
