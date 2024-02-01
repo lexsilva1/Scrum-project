6d15eb51ee23bb2a3e32536825e9e7d9409dc74b
JoseSousaCastro <152998913+JoseSousaCastro@users.noreply.github.com>
2024-02-01 14:28:10 +0000

    botoes finalizados, data e hora alterados (sem segundos)

--- homescript.js
+++ homescript.js
@@ -274,7 +274,8 @@ const displayTime = document.querySelector(".display-time");
 // Time
 function showTime() {
   let time = new Date();
-  displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: false });
+  let timeString = time.toLocaleTimeString("en-US", { hour12: false, hour: '2-digit', minute: '2-digit' });
+  displayTime.innerText = timeString;
   setTimeout(showTime, 1000);
 }
 
